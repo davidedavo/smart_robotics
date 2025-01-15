b93b83e5af5daf39048de6af070d83bcf47c261e
Davide Davoli <davidedavo@gmail.com>
2025-01-15 17:10:43 +0100

    Fix X11 forwarding

--- docker/run.sh
+++ docker/run.sh
@@ -5,15 +5,19 @@ code_dir="${parent_dir}/src"
 cache_dir="${parent_dir}/gazebo_cache"
 mkdir -p $cache_dir
 
+# X11 forwarding: https://www.baeldung.com/linux/docker-container-gui-applications 
+touch /tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge - 
 
 docker run -it --rm  \
     --gpus all \
     --privileged \
-    --env DISPLAY=$DISPLAY \
     -v $code_dir:/src \
     -v $cache_dir:/root/.gazebo \
-    -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume /tmp/.docker.xauth:/tmp/.docker.xauth \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --env DISPLAY=$DISPLAY \
+    --env XAUTHORITY=/tmp/.docker.xauth \
     --net=host \
     -w /src \
     ros-gazebo
