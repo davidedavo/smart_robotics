8fae2cee57140f5a68e04f02ba29700940bbd715
Davide Davoli <davidedavo@gmail.com>
2025-01-17 12:09:43 +0100

    Installed panda_simulator

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 parent_dir=$(dirname "$(pwd)")
-code_dir="${parent_dir}/src"
+workspace_dir="${parent_dir}/workspace"
 cache_dir="${parent_dir}/gazebo_cache"
 mkdir -p $cache_dir
 
@@ -10,14 +10,16 @@ touch /tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge - 
 
 docker run -it --rm  \
+    --name ros_gazebo \
     --gpus all \
+    --shm-size=2gb \
     --privileged \
-    -v $code_dir:/src \
+    -v $workspace_dir:/workspace \
     -v $cache_dir:/root/.gazebo \
     --volume /tmp/.docker.xauth:/tmp/.docker.xauth \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --env DISPLAY=$DISPLAY \
     --env XAUTHORITY=/tmp/.docker.xauth \
     --net=host \
-    -w /src \
+    -w /workspace \
     ros-gazebo
--- src/run_gazebo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source /opt/ros/foxy/setup.bash
-ros2 launch gazebo_ros gazebo.launch.py
