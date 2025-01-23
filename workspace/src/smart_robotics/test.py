7455d7ef115eaf9a0c6d256dc225198bf8ca7d27
Davide Davoli <davidedavo@gmail.com>
2025-01-23 14:37:25 +0100

    project world and test file

---
+++ workspace/src/smart_robotics/test.py
@@ -0,0 +1,45 @@
+import sys
+
+import numpy as np
+import rospy
+from panda_robot import PandaArm
+from geometry_msgs.msg import Pose
+
+def main():
+    rospy.init_node('panda_sim')
+
+    panda = PandaArm()
+    gripper = panda.get_gripper()
+    panda.untuck()
+    gripper.open()
+
+    target_pose = np.array([0.45645, -0.107404, 0.426083])
+    target_quat = np.array([1., 0., 0., 0.])
+
+    target_pose[-1] += 0.1
+
+    intermediate_pose = target_pose.copy()
+    intermediate_pose[-1] += 0.2
+    
+    ret, int_joints = panda.inverse_kinematics(intermediate_pose, target_quat)
+    if ret or True:
+        panda.move_to_joint_position(int_joints)
+
+    rospy.sleep(1)
+    
+    ret, tgt_joints = panda.inverse_kinematics(target_pose, target_quat)
+    if ret or True:
+        panda.move_to_joint_position(tgt_joints)
+    
+    rospy.sleep(2)
+
+    # Close the gripper to grasp the unit cube
+    # gripper.close()
+    gripper.apply_force(10.0)
+
+    panda.move_to_joint_position(int_joints)   
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
