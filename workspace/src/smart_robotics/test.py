d311b02b68b1ad877be9771ba51fb7a290a4b270
martinademarco <marti.dema@gmail.com>
2025-01-29 16:05:55 +0100

    change in friction, grasp function, cube mass..

--- workspace/src/smart_robotics/test.py
+++ workspace/src/smart_robotics/test.py
@@ -5,6 +5,14 @@ import rospy
 from panda_robot import PandaArm
 from geometry_msgs.msg import Pose
 
+
+# Imposta i parametri per la larghezza dell'oggetto e la forza
+object_width = 0.05  # Supponendo che la box abbia una larghezza di 5 cm
+grasp_force = 20.0   # Forza di presa in Newton
+grasp_speed = 0.05   # Velocità di chiusura del gripper in m/s
+
+
+
 def main():
     rospy.init_node('panda_sim')
 
@@ -34,8 +42,17 @@ def main():
     rospy.sleep(2)
 
     # Close the gripper to grasp the unit cube
-    # gripper.close()
-    gripper.apply_force(10.0)
+    #gripper.close()
+    #gripper.apply_force(10.0)
+    # Chiudi il gripper e afferra l'oggetto
+    success = gripper.grasp(width=object_width, force=grasp_force, speed=grasp_speed)
+
+    if success:
+        print("Oggetto afferrato con successo!")
+    else:
+        print("Impossibile afferrare l'oggetto.")
+
+    rospy.sleep(2)
 
     panda.move_to_joint_position(int_joints)   
 
